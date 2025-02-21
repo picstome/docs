81d9b78464eb2862201beab4e903cd0eba2241fa
Chema <neo22s@gmail.com>
2025-02-21 12:29:40 +0100

    Initial commit - VitePress setup

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,14 @@
+export default {
+  title: "Picstome.com",
+  description: "Documentation for Picstome",
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Guide", link: "/guide" },
+    ],
+    sidebar: [
+      { text: "Introduction", link: "/" },
+      { text: "Getting Started", link: "/guide" },
+    ],
+  },
+};
